786da49d9ecea24d07bc0ea0c30c196420cc9081
NaimunNahar14 <naimunnaharshoshi@gmail.com>
2023-05-04 05:38:39 -0700

    firebase.config

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -4,13 +4,14 @@ import { initializeApp } from "firebase/app";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+console.log('enviroment ',import.meta.env.VITE_apiKey)
 const firebaseConfig = {
-  apiKey: "AIzaSyC2zySWAEJrwzrj-9zKDvQa-yXfXrMCG08",
-  authDomain: "recipe-hunter-4a325.firebaseapp.com",
-  projectId: "recipe-hunter-4a325",
-  storageBucket: "recipe-hunter-4a325.appspot.com",
-  messagingSenderId: "1049171041545",
-  appId: "1:1049171041545:web:87fd750032c85e61edc7d8"
+  apiKey: import.meta.env.VITE_apiKey,
+  authDomain: import.meta.env.VITE_authDomain,
+  projectId: import.meta.env.VITE_projectId,
+  storageBucket: import.meta.env.VITE_storageBucket ,
+  messagingSenderId:import.meta.env.VITE_messagingSenderId,
+  appId: import.meta.env.VITE_appId
 };
 
 // Initialize Firebase
--- src/pages/Shared/Terms.jsx
+++ src/pages/Shared/Terms.jsx
@@ -4,7 +4,8 @@ import { Link } from 'react-router-dom';
 const Terms = () => {
     return (
         <div>
-            <h2>terms and conditions</h2>
+            <h2>Terms and Conditions</h2>
+            <p>These terms and conditions ("Terms") govern your use of our website ("Website"), which provides information and services related to chefs and recipes. By using our Website, you agree to these Terms and our Privacy Policy, which can be found on our Website.You may use our Website for lawful purposes only. You are prohibited from using our Website to violate any applicable laws, regulations, or third-party rights. You are responsible for all activities that occur under your account, and you must keep your account credentials secure and confidential.</p>
             <p>Go to <Link to='/register'>register</Link></p>
         </div>
     );
