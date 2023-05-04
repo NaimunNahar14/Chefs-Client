c76480a807507c09be33279c0ae1e20e5b3329af
NaimunNahar14 <naimunnaharshoshi@gmail.com>
2023-05-03 23:02:49 -0700

    fixed register

--- src/routes/Register.jsx
+++ src/routes/Register.jsx
@@ -2,13 +2,33 @@ import React from 'react';
 import { Button, Container } from 'react-bootstrap';
 import {  Link } from 'react-router-dom';
 import Form from 'react-bootstrap/Form';
+import { useContext } from 'react';
+import { AuthContext } from '../providers/AuthProvider';
 
 const Register = () => {
+    const { createUser } = useContext(AuthContext);
+    const handleRegister = event =>{
+        event.preventDefault();
+        const form = event.target;
+        const name = form.name.value;
+        const photo = form.photo.value;
+        const email = form.email.value;
+        const password = form.password.value;
+        console.log(name, photo, email, password)
+        createUser(email, password)
+        .then (result => {
+            const createdUser = result.user;
+            console.log(createdUser)
+        })
+        .catch(error =>{
+            console.log(error);
+        })
+    }
     return (
         <div>
            <Container className='w-25 mx-auto'>
             <h2 className='text-center'>Register</h2>
-                <Form>
+                <Form onSubmit={handleRegister}>
                 <Form.Group className="mb-3" controlId="formBasicEmail">
                         <Form.Label>Name</Form.Label>
                         <Form.Control type="text" name='name' placeholder="Enter Name" required />
